e1536e758bb87b9e9e2bb4182248c3390d6d4e3d
wdecoster <decosterwouter@gmail.com>
2019-02-19 13:47:03 +0100

    style changes

--- nanoplotter/compplots.py
+++ nanoplotter/compplots.py
@@ -15,28 +15,25 @@ def violin_or_box_plot(df, y, figformat, path, y_name,
     The x-axis should be divided based on the 'dataset' column,
     the y-axis is specified in the arguments
     """
-    violin_comp = Plot(
-        path=path + "NanoComp_" + y.replace(' ', '_') + '.' + figformat,
-        title="Comparing {}".format(y))
+    violin_comp = Plot(path=path + "NanoComp_" + y.replace(' ', '_') + '.' + figformat,
+                       title="Comparing {}".format(y))
     if y == "quals":
         violin_comp.title = "Comparing base call quality scores"
     if violin:
         logging.info("Nanoplotter: Creating violin plot for {}.".format(y))
-        ax = sns.violinplot(
-            x="dataset",
-            y=y,
-            data=df,
-            inner=None,
-            cut=0,
-            palette=palette,
-            linewidth=0)
+        ax = sns.violinplot(x="dataset",
+                            y=y,
+                            data=df,
+                            inner=None,
+                            cut=0,
+                            palette=palette,
+                            linewidth=0)
     else:
         logging.info("Nanoplotter: Creating box plot for {}.".format(y))
-        ax = sns.boxplot(
-            x="dataset",
-            y=y,
-            data=df,
-            palette=palette)
+        ax = sns.boxplot(x="dataset",
+                         y=y,
+                         data=df,
+                         palette=palette)
     if log:
         ticks = [10**i for i in range(10) if not 10**i > 10 * (10**np.amax(df[y]))]
         ax.set(yticks=np.log10(ticks),
@@ -53,13 +50,11 @@ def violin_or_box_plot(df, y, figformat, path, y_name,
 def output_barplot(df, figformat, path, title=None, palette=None):
     """Create barplots based on number of reads and total sum of nucleotides sequenced."""
     logging.info("Nanoplotter: Creating barplots for number of reads and total throughput.")
-    read_count = Plot(
-        path=path + "NanoComp_number_of_reads." + figformat,
-        title="Comparing number of reads")
-    ax = sns.countplot(
-        x="dataset",
-        data=df,
-        palette=palette)
+    read_count = Plot(path=path + "NanoComp_number_of_reads." + figformat,
+                      title="Comparing number of reads")
+    ax = sns.countplot(x="dataset",
+                       data=df,
+                       palette=palette)
     ax.set(ylabel='Number of reads',
            title=title or read_count.title)
     plt.xticks(rotation=30, ha='center')
@@ -67,18 +62,15 @@ def output_barplot(df, figformat, path, title=None, palette=None):
     read_count.save(format=figformat)
     plt.close("all")
 
-    throughput_bases = Plot(
-        path=path + "NanoComp_total_throughput." + figformat,
-        title="Comparing throughput in gigabases sequenced")
+    throughput_bases = Plot(path=path + "NanoComp_total_throughput." + figformat,
+                            title="Comparing throughput in gigabases sequenced")
     throughput = df.groupby('dataset')['lengths'].sum()
-    ax = sns.barplot(
-        x=list(throughput.index),
-        y=throughput / 1e9,
-        palette=palette,
-        order=df["dataset"].unique())
-    ax.set(
-        ylabel='Total gigabase sequenced',
-        title=title or throughput_bases.title)
+    ax = sns.barplot(x=list(throughput.index),
+                     y=throughput / 1e9,
+                     palette=palette,
+                     order=df["dataset"].unique())
+    ax.set(ylabel='Total gigabase sequenced',
+           title=title or throughput_bases.title)
     plt.xticks(rotation=30, ha='center')
     throughput_bases.fig = ax.get_figure()
     throughput_bases.save(format=figformat)
@@ -92,9 +84,8 @@ def compare_cumulative_yields(df, path, palette=None, title=None):
     dfs = check_valid_time_and_sort(df, "start_time").set_index("start_time")
 
     logging.info("Nanoplotter: Creating cumulative yield plots using {} reads.".format(len(dfs)))
-    cum_yield_gb = Plot(
-        path=path + "NanoComp_CumulativeYieldPlot_Gigabases.html",
-        title="Cumulative yield")
+    cum_yield_gb = Plot(path=path + "NanoComp_CumulativeYieldPlot_Gigabases.html",
+                        title="Cumulative yield")
     data = []
     for d, c in zip(df.dataset.unique(), palette):
         s = dfs.loc[dfs.dataset == d, "lengths"].cumsum().resample('10T').max() / 1e9
